ce1bdfb2ac4ed7cecbdc0c7a59f32f2b4d1edf40
Sunil <Admin@Sunil-PC>
2013-12-06 18:07:42 +0530

    Student Module

---
+++ GitWebProject/src/com/app/Student.java
@@ -0,0 +1,32 @@
+package com.app;
+
+public class Student {
+	private long id;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getMax() {
+		return max;
+	}
+
+	public void setMax(int max) {
+		this.max = max;
+	}
+
+	private String name;
+	private int max; 
+}
